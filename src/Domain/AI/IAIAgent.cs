91849e9ff26a83152cc079264cb2fc61c779b548
Be4Die <misha375375@gmail.com>
2024-03-19 09:00:06 +0400

    random ai

---
+++ src/Domain/AI/IAIAgent.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.AI;
+
+public interface IAIAgent
+{
+    public void MakeMove();
+}
---
+++ src/Domain/AI/RandomAgent.cs
@@ -0,0 +1,44 @@
+﻿using SeaBattle.Domain;
+
+namespace Domain.AI;
+
+internal sealed class RandomAgent : IAIAgent
+{
+    private readonly Board _board;
+
+    private readonly (int, int)[] _points;
+
+    private int _currentPointIndex = 0;
+
+    public RandomAgent(Board board)
+    {
+        _board = board;
+        var random = new Random();
+
+
+        _points = new (int, int)[_board.Width * _board.Height];
+
+        int i = 0;
+        for (int x = 0; x < _board.Width; x++)
+        {
+            for (int y = 0; y < _board.Height; y++)
+            {
+                _points[i] = (x, y);
+                i++;
+            }
+        }
+
+        random.Shuffle(_points);
+
+    }
+
+
+    public void MakeMove()
+    {
+        var (x,y) = GetRandomPoint();
+
+        _board.Hit((uint)x, (uint)y);
+    }
+
+    private (int, int) GetRandomPoint() => _points[_currentPointIndex++];
+}
